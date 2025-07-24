d9c5d95ad8bec4e895e0f57953fa18f20094dfbf
Soheil <hesarisoheil1999@example.com>
2025-07-24 09:55:58 +0330

    prepare db

--- apps/lefex/di/general_provider.py
+++ apps/lefex/di/general_provider.py
@@ -1,18 +1,20 @@
-from typing import ClassVar
+from typing import ClassVar, Any
 from typing import TypeVar
 from apps.lefex.config import Settings
 from hashlib import sha224
 
+
 class DependencyProvider(object):
 
-    _objects : ClassVar[dict[type, object]] = {}
+    _objects : ClassVar[dict[type, Any]] = {}
 
 
     @classmethod
-    def settings(cls):
+    def settings(cls) -> Settings:
 
         if Settings not in cls._objects:
             cls._objects[Settings] = Settings()
+            return cls._objects[Settings]
         else:
             return cls._objects[Settings]
 
@@ -33,4 +35,3 @@ class DependencyProvider(object):
     #
     #     return result
     #
-
